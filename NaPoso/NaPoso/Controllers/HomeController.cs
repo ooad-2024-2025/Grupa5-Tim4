fc077e39d0bd824ca9b8e034407b6d1dc445920b
Zinedin04 <zbalihodzi1@etf.unsa.ba>
2025-05-22 20:41:07 +0200

    Razdvojen view za klijenta i radnika

--- NaPoso/NaPoso/Controllers/HomeController.cs
+++ NaPoso/NaPoso/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NaPoso.Models;
 
@@ -6,18 +8,42 @@ namespace NaPoso.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(UserManager<IdentityUser> userManager, ILogger<HomeController> logger)
         {
+            _userManager = userManager;
             _logger = logger;
         }
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var roles = await _userManager.GetRolesAsync(user);
 
-        public IActionResult Index()
+                if (roles.Contains("Radnik"))
+                    return RedirectToAction("Radnik");
+                else if (roles.Contains("Klijent"))
+                    return RedirectToAction("Klijent");
+            }
+
+            return View(); // ako nije logovan, pokaži home stranicu
+        }
+
+        [Authorize(Roles = "Radnik")]
+        public IActionResult Radnik()
         {
             return View();
         }
 
+        [Authorize(Roles = "Klijent")]
+        public IActionResult Klijent()
+        {
+            return View();
+        }
+        
         public IActionResult Privacy()
         {
             return View();
