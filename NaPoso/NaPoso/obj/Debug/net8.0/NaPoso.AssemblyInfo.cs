df8dcb775f729d7cd872e2367a482827342ac016
spiki10 <idzafic1@etf.unsa.ba>
2025-06-04 10:54:31 +0200

    api key je public sad (educational purposes only pa je zato public + nije linkovano na moj racun

--- NaPoso/NaPoso/obj/Debug/net8.0/NaPoso.AssemblyInfo.cs
+++ NaPoso/NaPoso/obj/Debug/net8.0/NaPoso.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NaPoso")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2995b8bfd68315a6b857cacc250b8536fcd8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90e8f4dc499693667ababb3aac38fbacb4024dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("NaPoso")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NaPoso")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
